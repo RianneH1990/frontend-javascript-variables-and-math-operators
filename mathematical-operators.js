b491db3597a7b09b3e5104724323960bc8e0f91c
RianneH1990 <74492460+RianneH1990@users.noreply.github.com>
2021-03-25 14:48:37 +0100

    opdracht

--- mathematical-operators.js
+++ mathematical-operators.js
@@ -3,38 +3,63 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van 4 plus 5 in op.                           // geeft 9 wanneer gelogd
 // ==========================================
-
+let number1 = 4;
+let number2 = 5;
+let total = number1 + number2;
+console.log(total);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364 wanneer gelogd
 // ==========================================
-
+number1 = 7;
+number2 = 52;
+total = number1 * number2;
+console.log(total);
 
 // ==========================================
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49 wanneer gelogd
 // ==========================================
-
+number1 = 4;
+number2 = 3;
+let number3 = 7;
+const add = number1 + number2;
+total = add * number3;
+console.log(total);
 
 // ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12 wanneer gelogd
 // ==========================================
-
+number1 = 36;
+number2 = 6;
+number3 = 2;
+total = number1 / number2 * number3;
+console.log(total);
 
 // ==========================================
 // 5. Declareer twee variabelen en zet daarin respectievelijk de waardes 'zoet' en 'sappig'.
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig' wanneer gelogd
 // ==========================================
-
+const description1 = "zoet";
+const description2 = "sappig";
+const descriptionTogether = description1 + description2;
+console.log(descriptionTogether);
 
 // ==========================================
 // 6. Declareer een variabele voor jouw voornaam en een variabele voor jouw achternaam en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw volledige naam en plak bovenstaande variabelen aan elkaar.
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters' wanneer gelogd
 // ==========================================
-
+const name = "Rianne ";
+const surname = "Honders";
+const fullName = name + surname;
+console.log(fullName);
 
 // ==========================================
 // 7. Declareer een variabele voor jouw woonplaats en een variabele voor provincie en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg wanneer gelogd
 // ==========================================
+const city = "Vlaardingen, ";
+const province = "Zuid-Holland";
+const location = city + province;
+console.log(location);
